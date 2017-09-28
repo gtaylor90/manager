983873316f79ed2dfc3ce62029f330d06ef88ea1
gtaylor90 <graham.taylor.90@gmail.com>
2017-09-28 18:45:40 -0500

    set up

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { creatStore } from 'redux';
+
 
 class App extends Component {
   render() {
     return (
-    <Provider store={createStore()}>
-      <View>
-        <Text>
-          Hello
-        </Text>
-      </View>
-    </Provider>
-  );
+      <Provider store={creatStore()}>
+        <View>
+          <Text>
+            Hello
+          </Text>
+        </View>
+      </Provider>
+    );
   }
 }
 
