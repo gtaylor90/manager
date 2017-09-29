557b16d68acbd18b5c6e496265154be9264b3f1d
gtaylor90 <graham.taylor.90@gmail.com>
2017-09-28 19:49:49 -0500

    added firebase

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
 
 
 class App extends Component {
+  // placing firebase stuff here
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyCbmT0_PaxuhjkB1qoXuj8iyWExNKD5uMU',
+      authDomain: 'manager-b31ba.firebaseapp.com',
+      databaseURL: 'https://manager-b31ba.firebaseio.com',
+      projectId: 'manager-b31ba',
+      storageBucket: '',
+      messagingSenderId: '1035308760039'
+  };
+    firebase.initializeApp(config);
+  }
   render() {
     return (
-      <Provider store={createStore()}>
+      <Provider store={createStore(reducers)}>
         <View>
           <Text>
             Hello
