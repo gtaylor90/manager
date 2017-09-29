3ae1d0b6968bcfe7e4a4b74b3f7186445a8244ee
gtaylor90 <graham.taylor.90@gmail.com>
2017-09-28 20:40:25 -0500

    oof

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { emailChanged } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 class LoginForm extends Component {
+  onEmailChange(text) {
+    this.props.emailChanged(text);
+  }
   render() {
     return (
       <Card>
         <CardSection>
           <Input
-            label="email"
+            label="Email"
             placeholder="email@gmail.com"
+            onChangeText={this.onEmailChange.bind(this)}
           />
         </CardSection>
         <CardSection>
@@ -28,4 +34,4 @@ class LoginForm extends Component {
   }
 }
 
-export default LoginForm;
+export default connect(null, { emailChanged })(LoginForm);
