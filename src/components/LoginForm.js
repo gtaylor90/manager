2785304fbbfa4585c990261ca2d64241f0225f6e
gtaylor90 <graham.taylor.90@gmail.com>
2017-10-02 15:53:30 -0500

    got email kinda going right

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -15,11 +15,12 @@ class LoginForm extends Component {
             label="Email"
             placeholder="email@gmail.com"
             onChangeText={this.onEmailChange.bind(this)}
+            value={this.props.email}
           />
         </CardSection>
         <CardSection>
           <Input
-          secureTextEntry
+          secureText
           label="Password"
           placeholder="password"
           />
@@ -34,4 +35,10 @@ class LoginForm extends Component {
   }
 }
 
-export default connect(null, { emailChanged })(LoginForm);
+const mapStateToProps = state => {
+  return {
+    email: state.auth.email
+  };
+};
+
+export default connect(mapStateToProps, { emailChanged })(LoginForm);
--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -5,7 +5,8 @@ const INITIAL_STATE = { email: '' };
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case EMAIL_CHANGED:
-
+    // makes a new object, sets the email up as the input, and applies to state
+    return { ...state, email: action.payload };
     default:
       return state;
   }
