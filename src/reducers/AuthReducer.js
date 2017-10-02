3334fa807b7d288cb1c3e13c13e37dbf7a85ecab
gtaylor90 <graham.taylor.90@gmail.com>
2017-10-02 16:04:45 -0500

    state stuff for pw

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,12 +1,20 @@
-import { EMAIL_CHANGED } from '../actions/types';
+import {
+  EMAIL_CHANGED,
+  PASSWORD_CHANGED
+ } from '../actions/types';
 
-const INITIAL_STATE = { email: '' };
+const INITIAL_STATE = {
+  email: '',
+  password: ''
+ };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case EMAIL_CHANGED:
     // makes a new object, sets the email up as the input, and applies to state
-    return { ...state, email: action.payload };
+      return { ...state, email: action.payload };
+    case PASSWORD_CHANGED:
+      return { ...state, password: action.payload };
     default:
       return state;
   }
